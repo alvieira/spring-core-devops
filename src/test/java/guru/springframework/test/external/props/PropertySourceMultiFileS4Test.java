adab12dbb96956876c544038830b2aa33b04ccfa
Aloisio Vieira <aloisio.vieira@gmail.com>
2020-04-06 23:25:09 -0300

    multiple properties files

---
+++ src/test/java/guru/springframework/test/external/props/PropertySourceMultiFileS4Test.java
@@ -0,0 +1,28 @@
+package guru.springframework.test.external.props;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+import guru.springframework.test.jms.FakeJmsBroker;
+import guru.test.config.external.props.ExternalPropsMultiFileS4;
+
+@SpringBootTest(classes = ExternalPropsMultiFileS4.class)
+@ContextConfiguration
+public class PropertySourceMultiFileS4Test {
+
+	@Autowired
+	FakeJmsBroker fakeJmsBroker;
+
+	@Test
+	public void testPropsSet() throws Exception {
+		assertEquals("10.10.10.123", fakeJmsBroker.getUrl());
+		assertEquals(3330, fakeJmsBroker.getPort().intValue());
+		assertEquals("Ron", fakeJmsBroker.getUser());
+		assertEquals("&%$)(*&#^!@!@#$", fakeJmsBroker.getPassword());
+	}
+
+}
