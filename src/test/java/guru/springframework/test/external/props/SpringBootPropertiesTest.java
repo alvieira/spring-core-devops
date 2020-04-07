82748ea2d57b3726536c506d0dae3da985b3692c
Aloisio Vieira <aloisio.vieira@gmail.com>
2020-04-06 23:49:47 -0300

    Spring Boot application properties

---
+++ src/test/java/guru/springframework/test/external/props/SpringBootPropertiesTest.java
@@ -0,0 +1,30 @@
+package guru.springframework.test.external.props;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import guru.springframework.test.SpringBootJavaConfig;
+import guru.springframework.test.jms.FakeJmsBroker;
+
+@SpringBootTest(classes = SpringBootJavaConfig.class)
+@WebAppConfiguration
+@TestPropertySource("/application.properties")
+public class SpringBootPropertiesTest {
+	
+	@Autowired
+    FakeJmsBroker fakeJmsBroker;
+
+    @Test
+    public void testPropsSet() throws Exception {
+        assertEquals("10.10.10.123", fakeJmsBroker.getUrl());
+        assertEquals(3330, fakeJmsBroker.getPort().intValue());
+        assertEquals("Ron", fakeJmsBroker.getUser());
+        assertEquals("Burgundy", fakeJmsBroker.getPassword());
+    }
+
+}
