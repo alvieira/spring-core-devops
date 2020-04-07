fb796ff884af7dfc8f856316d7d6eb632556103d
Aloisio Vieira <aloisio.vieira@gmail.com>
2020-04-06 22:55:14 -0300

    Property source

---
+++ src/test/java/guru/springframework/SpringCoreDevopsApplicationTests.java
@@ -0,0 +1,17 @@
+package guru.springframework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+import com.guru.springframework.SpringCoreDevopsApplication;
+
+@SpringBootTest(classes = SpringCoreDevopsApplication.class)
+@ContextConfiguration
+class SpringCoreDevopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
